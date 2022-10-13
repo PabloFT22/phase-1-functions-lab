420a624bcd05d8453a8ad37779065a56f83a018b
Pablo Fuentes Tudela <pablo.fuentes.tudela@outlook.com>
2022-10-13 17:20:03 -0400

    commit tree 93dc7262c8fd8816edc564f308540eb2a5e8ebe2

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const scuberHeadquarters = 42;
+function distanceFromHqInBlocks(ending) {
+    if (ending < scuberHeadquarters) {
+        return scuberHeadquarters - ending
+    } else 
+        return ending - scuberHeadquarters;
+}
+distanceFromHqInBlocks()
+
+
+function distanceFromHqInFeet(feet) {
+    const result = distanceFromHqInBlocks(feet);
+    return result * 264;
+}
+distanceFromHqInFeet()
+
+//42-32
+function ddistanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+    const diffrence = (start - destination) * 264
+    return diffrence;
+    } else {
+        const diffrence2 = (destination - start) * 264
+        return diffrence2
+    }
+}
+distanceFromHqInBlocks()
